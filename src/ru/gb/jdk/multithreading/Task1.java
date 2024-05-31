e3eb57ffef22ca02144c0e62b5d847330d5094df
Remi776 <99408740+Remi776@users.noreply.github.com>
2024-05-31 20:25:33 +0300

    multithreading task1

---
+++ src/ru/gb/jdk/multithreading/Task1.java
@@ -0,0 +1,21 @@
+package ru.gb.jdk.multithreading;
+
+
+//    В рамках выполнения задачи необходимо:
+//    Создать два класс ObjectA, ObjectB
+//    Реализовать класс в котором два потока при запуске провоцируют DeadLock для объектов ObjectA, ObjectB
+public class Task1 {
+    public static void main(String[] args) {
+        Object objectA = new Object();
+        Object objectB = new Object();
+
+        Thread threadA = new Thread(() -> {
+            synchronized (objectA){
+                System.out.println("ThreadA");
+                synchronized (objectB){
+                    System.out.println("ThreadB");
+                }
+            }
+        });
+    }
+}
